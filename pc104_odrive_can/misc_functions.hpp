f377ca1bef9d991e5fddf099c762a566e41abe29
Sumantra Sharma <sumantra94@gmail.com>
2019-11-18 17:06:09 +0800

    changed robot leg data structure

--- pc104_odrive_can/misc_functions.hpp
+++ pc104_odrive_can/misc_functions.hpp
@@ -3,4 +3,4 @@
 
 int binary_to_decimal(std::string binaryString);
 void bit_masking(&can_frame_odrive can_frame);
-void sort_can_node_id(std::vector<robot_leg> &legs,can_frame_odrive &msg);
+void sort_can_node_id(std::vector<std::vector<odrive_motor>> &legs,can_frame_odrive &msg);
