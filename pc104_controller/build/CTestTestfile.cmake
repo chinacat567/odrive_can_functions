c8bb96c145944d0badbd212fdd4b75003340328f
Sumantra Sharma <sumantra94@gmail.com>
2019-12-10 12:22:43 +0800

    updated CMakeLists.txt

--- pc104_controller/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/codecraft94/Documents/quadruped/pc104_controller
-# Build directory: /home/codecraft94/Documents/quadruped/pc104_controller/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
