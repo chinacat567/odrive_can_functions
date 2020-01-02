7eb0279b6918aca7d39be6a6154be9dc696fd86e
Sumantra Sharma <sumantra94@gmail.com>
2020-01-02 19:29:10 +0800

    P current control

---
+++ pc104_controller/build/CMakeFiles/526_test.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/codecraft94/Documents/nus_quadruped/pc104_controller/src/526_test.cpp" "/home/codecraft94/Documents/nus_quadruped/pc104_controller/build/CMakeFiles/526_test.dir/src/526_test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ANALOG_SCALE=400"
+  "CURR_FF=3"
+  "K_P=0.00125"
+  "MOTOR_COUNT=1"
+  "N_READ=1"
+  "N_WRITE=1"
+  "RT_PERIOD_1=2000000"
+  "RT_PERIOD_2=1000000"
+  "SCHED_P_1=81"
+  "SCHED_P_2=80"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/codecraft94/Documents/nus_quadruped/pc104_controller/build/CMakeFiles/controller_lib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ pc104_controller/build/CMakeFiles/controller_lib.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libcontroller_lib.a"
+)
